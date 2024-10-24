4c1f1f55b795d45303ebae9c3fbf1bfe32713f3b
zheer barzan <zheerbarzan85@outlook.com>
2024-10-24 20:22:45 +0300

    it works it fucking works thank the god above

--- lidarTest/ContentView.swift
+++ lidarTest/ContentView.swift
@@ -3,15 +3,41 @@ import ARKit
 import RealityKit
 
 struct ContentView: View {
-    var arView = ARView(frame: .zero)
-
+    @State private var lidarWorks = false
+    @State private var isChecked = false
+    
     var body: some View {
-        ARViewContainer(arView: arView)
-            .edgesIgnoringSafeArea(.all)
-            .onAppear {
-                ARSessionManager.setupARSession(arView: arView)
+        VStack {
+            Text(isChecked ? (lidarWorks ? "yahooo!" : "LiDAR sensor does not work.") : "Checking LiDAR sensor...")
+                .padding()
+            
+            Button(action: {
+                checkLiDAR()
+            }) {
+                Text("Check LiDAR")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
             }
+        }
+        .onAppear {
+            checkLiDAR()
+        }
+    }
+    
+    func checkLiDAR() {
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            lidarWorks = true
+        } else {
+            lidarWorks = false
+        }
+        isChecked = true
     }
 }
 
-
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
