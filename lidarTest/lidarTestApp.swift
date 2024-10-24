1bb8d0071be61da4f0b8c073b162cb1673b9a1d4
zheer barzan <zheerbarzan85@outlook.com>
2024-10-24 19:40:06 +0300

    Initial Commit

---
+++ lidarTest/lidarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  lidarTestApp.swift
+//  lidarTest
+//
+//  Created by Zheer Barzan on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct lidarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lidarTestTests/lidarTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  lidarTestTests.swift
+//  lidarTestTests
+//
+//  Created by Zheer Barzan on 24/10/24.
+//
+
+import Testing
+@testable import lidarTest
+
+struct lidarTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
